f145e544230be8ffca9047a62d9e533f6d48aab7
mfisher1991 <83640691+mfisher1991@users.noreply.github.com>
2021-05-17 10:39:16 -0700

    third commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let cells = document.querySelectorAll('.row>div');
+
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', cellClicked);
+}
+
+function cellClicked() {
+    event.target.textContent = 'x'
+    if () !(x = 0)
+
+}
+
+// HINTS!
+// if else statements are your friend!
+cells[2].textContent
